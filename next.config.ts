47b4a831313da5c8c40809ce37fb9830f3c96162
Rahul Gosavi <rahulgosavi624@gmail.com>
2026-01-14 16:23:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
+  api: {
+    bodyParser: {
+        sizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
